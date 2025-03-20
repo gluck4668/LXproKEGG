3e2eb327d21fcbad1ec944a079938719ad9a81ce
gluck4668 <109885777+gluck4668@users.noreply.github.com>
2025-03-20 11:34:23 +0800

    Add files via upload

--- LXproKEGG_example 2023-12-5.R
+++ LXproKEGG_example 2023-12-5.R
@@ -14,9 +14,10 @@ data(protein_data)
 
 rm(list=ls())
 
-devtools::load_all()
+# devtools::load_all()
 
-protein_data="protein_data.xlsx" # The data should be a list of protein UNIPROT, or three list including Protein_Uniprot, FC and pvalue
+protein_data="protein_list.xlsx" # The data should be a list of protein UNIPROT,
+                                 # or three list including Protein_Uniprot, FC and pvalue
 
 LXproKEGG(protein_data)
 
